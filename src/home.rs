4cd4ab92a53753d823c491695c20837c6e872c2e
Pierce Thompson <insprill@gmail.com>
2022-09-14 03:21:33 -0400

    Implement basic home page

--- src/home.rs
+++ src/home.rs
@@ -1,6 +1,9 @@
-use actix_web::{get, HttpResponse, Responder};
+use std::io;
+
+use actix_files::NamedFile;
+use actix_web::{get};
 
 #[get("/")]
-pub async fn home() -> impl Responder {
-    HttpResponse::Ok().body("home")
+pub async fn home() -> io::Result<NamedFile> {
+    Ok(NamedFile::open("static/home.html")?)
 }
