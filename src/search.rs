e66f5054023567ceab412c944153b8836bac6c73
Pierce Thompson <insprill@gmail.com>
2022-09-14 03:42:29 -0400

    Implement fetching genius search results

--- src/search.rs
+++ src/search.rs
@@ -1,6 +1,19 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, HttpResponse, Responder, web};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct SearchQuery {
+    q: String,
+}
 
 #[get("/search")]
-pub async fn search() -> impl Responder {
-    HttpResponse::Ok().body("search")
+pub async fn search(info: web::Query<SearchQuery>) -> impl Responder {
+    let body = reqwest::Client::new()
+        .get(format!("https://api.genius.com/search?q={}", info.q))
+        .header("Authorization", format!("Bearer {}", std::env::var("AUTH_TOKEN").unwrap()))
+        .send()
+        .await
+        .unwrap().text()
+        .await;
+    HttpResponse::Ok().body(body.unwrap())
 }
