50ad9122bbfe0faad865d77c0a8edbd86b45aef3
Pierce Thompson <insprill@gmail.com>
2022-09-18 05:35:58 -0400

    Pretty print song views

--- src/search.rs
+++ src/search.rs
@@ -61,3 +61,13 @@ struct GeniusStats {
     pageviews: Option<i32>,
 }
 // endregion
+
+fn pretty_format_num(num: i32) -> String {
+    if num >= 1_000_000 {
+        format!("{:.1}M", num as f32 / 1_000_000.0)
+    } else if num >= 1_000 {
+        format!("{}K", num / 1_000)
+    } else {
+        format!("{}", num)
+    }
+}
