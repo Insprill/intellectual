5b0a8c96511a3b583ddcb21ede84194fd714ee4d
Pierce Thompson <insprill@gmail.com>
2022-11-24 20:08:42 -0500

    Switch to clap's derive API

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::process::exit;
 
 use actix_web::{http::StatusCode, middleware, App, HttpServer};
-use clap::{Arg, Command};
+use clap::{arg, command, Parser};
 use log::{error, info, LevelFilter};
 use simplelog::{ColorChoice, CombinedLogger, Config, TermLogger, TerminalMode};
 
@@ -16,49 +16,29 @@ mod search;
 mod templates;
 mod utils;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Sets the address to listen on
+    #[arg(short, long, default_value = "localhost")]
+    address: String,
+
+    /// Sets the port to listen on. Will be overriden by the PORT env var if present
+    #[arg(short, long, default_value_t = 8080)]
+    port: u16,
+
+    /// The amount of HTTP workers to use. 0 to equal physical CPU cores
+    #[arg(short, long, default_value_t = 0)]
+    workers: usize,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let matches = Command::new("intellectual")
-        .version(env!("CARGO_PKG_VERSION"))
-        .about("Alternate front-end for Genius written in Rust")
-        .arg(
-            Arg::new("address")
-                .short('a')
-                .long("address")
-                .value_name("ADDRESS")
-                .help("Sets the address to listen on")
-                .default_value("0.0.0.0")
-                .num_args(1),
-        )
-        .arg(
-            Arg::new("port")
-                .short('p')
-                .long("port")
-                .value_name("PORT")
-                .help("Sets the port to listen on")
-                .default_value("8080")
-                .num_args(1),
-        )
-        .arg(
-            Arg::new("workers")
-                .short('w')
-                .long("workers")
-                .value_name("WORKERS")
-                .help("The amount of HTTP workers to use. 0 to equal physical CPU cores")
-                .default_value("0")
-                .num_args(1),
-        )
-        .get_matches();
+    let args = Args::parse();
 
-    let address = matches.get_one::<String>("address").unwrap().as_str();
     let port = std::env::var("PORT")
-        .unwrap_or_else(|_| matches.get_one::<String>("port").unwrap().to_string())
+        .unwrap_or_else(|_| args.port.to_string())
         .parse::<u16>()
-        .unwrap();
-    let workers = matches
-        .get_one::<String>("workers")
-        .unwrap_or(&"0".to_string())
-        .parse::<usize>()
         .unwrap();
 
     CombinedLogger::init(vec![TermLogger::new(
@@ -77,7 +57,7 @@ async fn main() -> std::io::Result<()> {
     info!(
         "Running Intellectual v{}, listening on {}:{}!",
         env!("CARGO_PKG_VERSION"),
-        address,
+        args.address,
         port
     );
 
@@ -110,9 +90,9 @@ async fn main() -> std::io::Result<()> {
             .service(resource::font)
     });
 
-    if workers > 0 {
-        server = server.workers(workers);
+    if args.workers > 0 {
+        server = server.workers(args.workers);
     }
 
-    server.bind((address, port))?.run().await
+    server.bind((args.address, port))?.run().await
 }
