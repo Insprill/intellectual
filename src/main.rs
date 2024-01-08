4fe4077ca43f1db072c5f9e5eae0be661d281435
Pierce Thompson <insprill@gmail.com>
2024-01-08 02:32:07 -0500

    Fix theme changes not applying due to caching

--- src/main.rs
+++ src/main.rs
@@ -68,6 +68,8 @@ async fn main() -> std::io::Result<()> {
 
     let args = Args::parse();
 
+
+
     let port = std::env::var("PORT")
         .unwrap_or_else(|_| args.port.to_string())
         .parse::<u16>()
@@ -91,7 +93,6 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Compress::default())
             .wrap(
                 middleware::DefaultHeaders::new()
-                    .add(("Cache-Control", "private, max-age=604800"))
                     .add(("Referrer-Policy", "no-referrer"))
                     .add(("X-Frame-Options", "DENY"))
                     .add(("X-Content-Type-Options", "nosniff"))
--- src/templates.rs
+++ src/templates.rs
@@ -4,5 +4,13 @@ use askama::Template;
 pub fn template(t: impl Template) -> HttpResponse {
     HttpResponse::Ok()
         .append_header(("Content-Type", "text/html; charset=utf-8"))
+        // Caching Setup
+        // Since Cloudflare ignores Vary headers, we can't publically cache all pages since only
+        // the last-cached theme would be shown to users. Instead, we privately cache all pages in the
+        // browser, which does handle the Vary header correctly. If we didn't have the Vary header,
+        // when a user changes themes, it wouldn't be applied to previously visited pages (e.g. the
+        // home page) until the browser requests the page from the server again.
+        .append_header(("Vary", "settings"))
+        .append_header(("Cache-Control", "private, max-age=604800"))
         .body(t.render().unwrap_or_default())
 }
