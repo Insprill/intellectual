11cc254cac43ea0d4350541c82c968ba600865c6
Pierce Thompson <insprill@gmail.com>
2024-01-10 00:10:08 -0500

    Don't set version metadata if we aren't in a Git repo

--- build.rs
+++ build.rs
@@ -6,19 +6,27 @@ fn main() {
     let cargo_version = env!("CARGO_PKG_VERSION");
     let mut version = cargo_version.to_string();
 
-    let is_repo_dirty = is_repo_dirty();
-    if !is_git_tagged() || is_repo_dirty {
-        version.push_str("+rev.");
-        version.push_str(&get_git_hash()[..7]);
+    if is_git_repo() {
+        let is_repo_dirty = is_repo_dirty();
+        if !is_git_tagged() || is_repo_dirty {
+            version.push_str("+rev.");
+            version.push_str(&get_git_hash()[..7]);
 
-        // Add some randomness if dirty to avoid the browser caching resources while iterating.
-        if is_repo_dirty {
-            version.push_str("-dirty-");
-            version.push_str(&generate(4, "abcdefghijklmnopqrstuvwxyz0123456789"));
+            // Add some randomness if dirty to avoid the browser caching resources while iterating.
+            if is_repo_dirty {
+                version.push_str("-dirty-");
+                version.push_str(&generate(4, "abcdefghijklmnopqrstuvwxyz0123456789"));
+            }
         }
+    } else {
+        println!("Not a Git repo! Skipping version metadata");
     }
 
     println!("cargo:rustc-env=IN_VERSION={}", version);
+}
+
+fn is_git_repo() -> bool {
+    Command::new("git").args(["status"]).status().is_ok()
 }
 
 fn get_git_hash() -> String {
