97dc74bc5689dd9014d927e656e46e4ba5ca72d4
Pierce Thompson <insprill@gmail.com>
2024-01-10 23:14:40 -0500

    Cleanup image endpoint code

--- src/api.rs
+++ src/api.rs
@@ -1,12 +1,13 @@
 use actix_web::{
     get,
     http::{header::ContentEncoding, StatusCode},
-    web, HttpResponse, Responder, Result,
+    web, HttpResponse, Responder,
 };
 use awc::{error::HeaderValue, http::header};
 use serde::Deserialize;
 
 use crate::genius::{self, SubDomain};
+use crate::Result;
 
 #[derive(Debug, Deserialize)]
 pub struct UrlQuery {
@@ -15,26 +16,25 @@ pub struct UrlQuery {
 
 #[get("/api/image")]
 pub async fn image(info: web::Query<UrlQuery>) -> Result<impl Responder> {
-    match info.url.split('/').last() {
-        Some(img_path) => {
-            let (status, body, headers) =
-                genius::get_raw(SubDomain::Images, img_path, None).await?;
+    let img_path = match info.url.split('/').last() {
+        Some(path) => path,
+        None => return Ok(HttpResponse::BadRequest().finish()),
+    };
 
-            if status != StatusCode::OK {
-                return Ok(HttpResponse::build(status).finish());
-            }
+    let (status, body, headers) = genius::get_raw(SubDomain::Images, img_path, None).await?;
 
-            Ok(HttpResponse::Ok()
-                .append_header(("Cache-Control", "public, max-age=31536000, immutable"))
-                .append_header((
-                    "Content-Type",
-                    headers
-                        .get(header::CONTENT_TYPE)
-                        .unwrap_or(&HeaderValue::from_static("application/octet-stream")),
-                ))
-                .insert_header(ContentEncoding::Identity)
-                .body(body))
-        }
-        None => Ok(HttpResponse::BadRequest().finish()),
+    if status != StatusCode::OK {
+        return Ok(HttpResponse::build(status).finish());
     }
+
+    Ok(HttpResponse::Ok()
+        .append_header(("Cache-Control", "public, max-age=31536000, immutable"))
+        .append_header((
+            "Content-Type",
+            headers
+                .get(header::CONTENT_TYPE)
+                .unwrap_or(&HeaderValue::from_static("application/octet-stream")),
+        ))
+        .insert_header(ContentEncoding::Identity)
+        .body(body))
 }
