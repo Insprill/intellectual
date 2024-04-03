b5548d13ca79ad01828f4a817bec1fe051c7e8e6
Pierce Thompson <insprill@gmail.com>
2024-04-03 02:05:04 -0400

    Match Genius' album path

--- src/album.rs
+++ src/album.rs
@@ -2,9 +2,8 @@ use crate::genius::{self, GeniusAlbumResponse};
 use crate::settings::{settings_from_req, Settings};
 use crate::utils;
 use actix_web::HttpRequest;
-use actix_web::{get, web, Responder, Result};
+use actix_web::{get, Responder, Result};
 use askama::Template;
-use serde::Deserialize;
 
 use crate::genius::GeniusAlbum;
 use crate::templates::template;
@@ -16,18 +15,11 @@ struct AlbumTemplate {
     album: GeniusAlbum,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct AlbumQuery {
-    path: String,
-}
-
-#[get("/album")]
-pub async fn album(req: HttpRequest, info: web::Query<AlbumQuery>) -> Result<impl Responder> {
-    let album_res = genius::extract_data::<GeniusAlbumResponse>(&utils::ensure_path_prefix(
-        "albums", &info.path,
-    ))
-    .await?;
-    let mut album = album_res.album;
+#[get("/albums/{name:.*}")]
+pub async fn album(req: HttpRequest) -> Result<impl Responder> {
+    let mut album = genius::extract_data::<GeniusAlbumResponse>(req.path())
+        .await?
+        .album;
 
     album.tracks = Some(genius::get_album_tracks(album.id).await?);
 
