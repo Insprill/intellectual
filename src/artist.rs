98527ef949c276838a8b2631617a60baede56c88
Pierce Thompson <insprill@gmail.com>
2024-04-03 01:42:29 -0400

    Match Genius' artist path

--- src/artist.rs
+++ src/artist.rs
@@ -1,10 +1,9 @@
 use crate::settings::{settings_from_req, Settings};
 use crate::utils;
-use actix_web::{get, web, HttpRequest, Responder, Result};
+use actix_web::{get, HttpRequest, Responder, Result};
 use askama::Template;
 use lazy_regex::*;
 use regex::Regex;
-use serde::Deserialize;
 
 use crate::genius::{self, GeniusArtist};
 use crate::genius::{GeniusArtistResponse, SortMode};
@@ -13,7 +12,7 @@ use crate::templates::template;
 static GENIUS_IMAGE_URL: &str = "https://images.genius.com/";
 static GENIUS_BASE_PATTERN: Lazy<Regex> = lazy_regex!(r#"https?://\w*.?genius\.com/"#);
 static GENIUS_ALBUMS_PATTERN: Lazy<Regex> = lazy_regex!(r#"https?://\w*.?genius\.com/albums/"#);
-static GENIUS_ARTIST_PATTERN: Lazy<Regex> = lazy_regex!(r#"https?://\w*.?genius\.com/artists/"#);
+static GENIUS_ARTIST_PATTERN: Lazy<Regex> = lazy_regex!(r#"https?://\w*.?genius\.com/"#);
 
 #[derive(Template)]
 #[template(path = "artist.html")]
@@ -22,20 +21,13 @@ struct ArtistTemplate {
     artist: GeniusArtist,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct ArtistQuery {
-    path: String,
-}
-
 const MAX_SONGS: u8 = 5;
 
-#[get("/artist")]
-pub async fn artist(req: HttpRequest, info: web::Query<ArtistQuery>) -> Result<impl Responder> {
-    let artist_res = genius::extract_data::<GeniusArtistResponse>(&utils::ensure_path_prefix(
-        "artists", &info.path,
-    ))
-    .await?;
-    let mut artist = artist_res.artist;
+#[get("/artists/{name}")]
+pub async fn artist(req: HttpRequest) -> Result<impl Responder> {
+    let mut artist = genius::extract_data::<GeniusArtistResponse>(req.path())
+        .await?
+        .artist;
 
     artist.popular_songs =
         Some(genius::get_artist_songs(artist.id, SortMode::Popularity, MAX_SONGS).await?);
@@ -59,7 +51,7 @@ fn rewrite_links(html: &str) -> String {
         &format!("/api/image?url={}", GENIUS_IMAGE_URL),
     ); // Images
     let html = GENIUS_ALBUMS_PATTERN.replace_all(&html, "/album?path=albums/"); // Albums
-    let html = GENIUS_ARTIST_PATTERN.replace_all(&html, "/artist?path=artists/"); // Artists
+    let html = GENIUS_ARTIST_PATTERN.replace_all(&html, ""); // Artists
     let html = GENIUS_BASE_PATTERN.replace_all(&html, "/lyrics?path=/"); // Lyrics
     html.to_string()
 }
