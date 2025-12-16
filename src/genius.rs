ae3a4fe25fbd22db898ea9bffa21dce9647b536a
Pierce Thompson <pierce@insprill.net>
2025-12-16 16:16:42 -0500

    Set the awc user agent to a standard chrome windows one

--- src/genius.rs
+++ src/genius.rs
@@ -170,6 +170,7 @@ async fn build_req(
 
     let res = Client::builder()
         .timeout(Duration::from_secs(TIMEOUT_SECS))
+        .add_default_header(("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"))
         .finish()
         .get(url)
         .send()
